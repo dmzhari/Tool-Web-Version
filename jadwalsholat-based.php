c7fdb3e748f8fa8ae358fca1af37fc0c48649d49
Dmz Hari <45622512+dmzhari@users.noreply.github.com>
2020-11-19 22:37:49 +0700

    Update jadwalsholat-based.php

--- jadwalsholat-based.php
+++ jadwalsholat-based.php
@@ -23,19 +23,17 @@ if (isset($_POST['namakota']) && !empty($_POST['namakota'])) {
 	$sekarang = date("Y-m-d");
 	$api = file_get_contents("https://api.banghasan.com/sholat/format/json/kota/nama/$filter");
 	$json = json_decode($api, true);
-	foreach ($json['kota'] as $key) {
-		$nama = $key['id'];
-		$jadwal = file_get_contents("https://api.banghasan.com/sholat/format/json/jadwal/kota/$nama/tanggal/$sekarang");
-		$jsond = json_decode($jadwal, true);
-		echo "<br>Nama Kota : ".$key['nama']."<br>";
-		echo "Ashar : ".$jsond['jadwal']['data']['ashar']."<br>";
-		echo "Dzuhur : ".$jsond['jadwal']['data']['dzuhur']."<br>";
-		echo "Imsak : ".$jsond['jadwal']['data']['imsak']."<br>";
-		echo "Maghrib : ".$jsond['jadwal']['data']['maghrib']."<br>";
-		echo "Subuh : ".$jsond['jadwal']['data']['subuh']."<br>";
-		//echo "Tanggal : ".$jsond['jadwal']['data']['tanggal']."<br>";
-		echo "Terbit : ".$jsond['jadwal']['data']['terbit']."<br><br>";
-	}
+	$nama = $json['kota']['0']['id'];
+	$jadwal = file_get_contents("https://api.banghasan.com/sholat/format/json/jadwal/kota/$nama/tanggal/$sekarang");
+	$jsond = json_decode($jadwal, true);
+	echo "<br>Nama Kota : ".$json['kota']['0']['nama']."<br>";
+	echo "Ashar : ".$jsond['jadwal']['data']['ashar']."<br>";
+	echo "Dzuhur : ".$jsond['jadwal']['data']['dzuhur']."<br>";
+	echo "Imsak : ".$jsond['jadwal']['data']['imsak']."<br>";
+	echo "Maghrib : ".$jsond['jadwal']['data']['maghrib']."<br>";
+	echo "Subuh : ".$jsond['jadwal']['data']['subuh']."<br>";
+	//echo "Tanggal : ".$jsond['jadwal']['data']['tanggal']."<br>";
+	echo "Terbit : ".$jsond['jadwal']['data']['terbit']."<br><br>";
 }
 else if (isset($_POST['semuakota']) && !empty($_POST['semuakota'])) {
 	$kotall = file_get_contents("https://api.banghasan.com/sholat/format/json/kota");
